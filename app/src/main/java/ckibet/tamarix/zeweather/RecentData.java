03876cc19fbffdd9c5f66c4c924167c634d7d496
Clement kibet <clemkibet@gmail.com>
2020-07-31 21:57:51 +0300

    More changes

---
+++ app/src/main/java/ckibet/tamarix/zeweather/RecentData.java
@@ -0,0 +1,14 @@
+package ckibet.tamarix.zeweather;
+
+public class RecentData {
+
+    public static final String TABLE_NAME = "weather_data";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_CITY = "city";
+    public static final String COLUMN_country = "country";
+    public static final String COLUMN_WIND = "wind";
+    public static final String COLUMN_HUMIDITY = "humidity";
+//    public static final String COLUMN_ID = "id";
+
+}
