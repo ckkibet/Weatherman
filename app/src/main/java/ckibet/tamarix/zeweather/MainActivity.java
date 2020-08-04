306ef6940c9d9936f8ec3b5154c9a39157eec3f1
Clement kibet <clemkibet@gmail.com>
2020-08-04 16:54:06 +0300

    Sign-up aka Registration Added

--- app/src/main/java/ckibet/tamarix/zeweather/MainActivity.java
+++ app/src/main/java/ckibet/tamarix/zeweather/MainActivity.java
@@ -1,30 +1,26 @@
 package ckibet.tamarix.zeweather;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEmailAddress;
+    EditText mPassword;
+    Button mSignUp;
+    Button mSignin;
+    TextView mForgotPassword;
 
     private GoogleSignInClient mGoogleSignInClient;
     private static int RC_SIGN_IN = 10;
@@ -49,75 +45,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEmailAddress = findViewById(R.id.email_address);
+        mPassword = findViewById(R.id.password);
+        mSignin = findViewById(R.id.sign_in);
+        mSignUp = findViewById(R.id.sign_up);
+        mForgotPassword = findViewById(R.id.forgot_password);
         mAuth = FirebaseAuth.getInstance();
 
-        createRequest();
-        findViewById(R.id.sign_in).setOnClickListener(new View.OnClickListener() {
+        signin();
+        signup();
+        forgotPassword();
+
+    }
+
+    private void signin() {
+
+
+
+    }
+
+    private void signup() {
+        mSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signIn();
+                startActivity(new Intent(getApplicationContext(), Sign_up.class));
+                finish();
             }
         });
 
-    }
-
-    private void createRequest() {
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 
     }
 
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
+    private void forgotPassword() {
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                // ...
-                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-
-                            Intent intent = new Intent(getApplicationContext(), Weather.class);
-                            startActivity(intent);
-
-                        } else {
-                            Toast.makeText(MainActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
-
-                        }
-
-                        // ...
-                    }
-                });
     }
 
 
---
+++ app/src/main/java/ckibet/tamarix/zeweather/Sign_up.java
@@ -0,0 +1,96 @@
+package ckibet.tamarix.zeweather;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Sign_up extends AppCompatActivity {
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (firebaseAuth.getCurrentUser() !=null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+    }
+
+    private EditText mEmailaddress, mPassword, mRepeatPassword;
+    private FirebaseAuth firebaseAuth;
+    Button mSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mEmailaddress = findViewById(R.id.email_address);
+        mPassword = findViewById(R.id.password);
+        mRepeatPassword = findViewById(R.id.repeat_password);
+        mSignup = findViewById(R.id.sign_up);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        mSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+                final ProgressDialog signupProgress = new ProgressDialog(Sign_up.this);
+                signupProgress.setMessage("Creating User");
+            }
+        });
+
+
+    }
+
+    private void registerUser() {
+        String email = mEmailaddress.getText().toString().trim();
+        String password = mPassword.getText().toString();
+        String rep_password = mRepeatPassword.getText().toString();
+
+
+        if (TextUtils.isEmpty(email)){
+            mEmailaddress.setError("Email is required");
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            mEmailaddress.setError("Password is required");
+            return;
+        }
+        if (password.length()<6){
+            mPassword.setError("Password must be more than 6 characters");
+            return;
+        }
+        if(!TextUtils.equals(password,rep_password)){
+            mRepeatPassword.setError("Password does not match");
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(Sign_up.this, "User Created!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }else  {
+                    Toast.makeText(Sign_up.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
